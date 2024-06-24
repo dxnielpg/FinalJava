c42e774eaa13fdbdc60c1730e8f7f31ade790f9d
Jhonathan dos Santos Tomkiv <141834841+jhonazin@users.noreply.github.com>
2024-06-24 10:58:49 -0300

    Implementação do historico de emprestimo

---
+++ FinalJava/src/BibliotecaPersistencia.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.List;
+
+public class BibliotecaPersistencia<T> {
+
+    public void salvar(List<T> lista, String arquivo) throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(arquivo))) {
+            oos.writeObject(lista);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<T> carregar(String arquivo) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(arquivo))) {
+            return (List<T>) ois.readObject();
+        }
+    }
+}
